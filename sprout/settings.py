23703cfe4a4037b629b59528f3ec66c2a21de64c
Katie McLaughlin <katie@glasnt.com>
2019-11-04 08:38:00 +1100

    backlog

--- sprout/settings.py
+++ sprout/settings.py
@@ -15,6 +15,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# Enable Django security precautions if *not* running locally
+if "0.0.0.0" not in ALLOWED_HOSTS:
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = "DENY"
+
 
 # Application definition
 
